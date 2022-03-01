061b5be8264902620028260ec686c8a497732675
Kelvin Ly <kelvin.ly1618@gmail.com>
2022-02-28 21:49:09 -0500

    Add new Digikey BOM generator for KiCAD 6

--- gen_digikey.py
+++ gen_digikey.py
@@ -15,31 +15,34 @@ def main():
   digikey_idx = None
   count_idx = None
   components = []
+  # TODO skip until it sees "Collated Components"
   with open(args.input, 'r') as f:
     reader = csv.reader(f)
-    parts = next(reader)
-    for i, part in enumerate(parts):
-      if 'Digikey' == part:
-        digikey_idx = i
-      elif 'Quantity' == part:
-        count_idx = i
-
-    if digikey_idx is None or count_idx is None:
-      print("Unable to find headers")
-      return
-
-    for i, parts in enumerate(reader):
-      if len(parts) < max(digikey_idx, count_idx):
-        print("[WARN] line {} is missing fields".format(i + 1))
+    found_start = False
+    for line_number, line in enumerate(reader):
+      if not found_start:
+        if any(["Collated" in p for p in line]):
+          #print("header at {}".format(line_number + 1))
+          found_start = True
         continue
-      part_num = parts[digikey_idx]
-      count = parts[count_idx]
+      if digikey_idx is None or count_idx is None:
+        for i, part in enumerate(line):
+          if 'Digikey' == part:
+            digikey_idx = i
+          elif 'Qty' == part:
+            count_idx = i
+        continue
+      if len(line) < max(digikey_idx, count_idx):
+        print("[WARN] line {} is missing fields".format(line_number + 1))
+        continue
+      part_num = line[digikey_idx]
+      count = line[count_idx]
       if len(part_num) == 0:
-        print("[WARN] line {} is missing part number".format(i + 1))
+        print("[WARN] line {} is missing part number".format(line_number + 1))
         print(parts)
         continue
       if count == 0:
-        print("[WARN] line {} has zero quantity".format(i + 1))
+        print("[WARN] line {} has zero quantity".format(line_number + 1))
       if part_num == 'NoPart':
         continue
       components.append((part_num, count))
@@ -48,7 +51,7 @@ def main():
     # header is necessary if you don't want to miss the first part
     f.write("Digikey,Quantity\n")
     for c in components:
-      f.write("{},{}\n".format(c[0], multiplier*int(c[1])))
+      f.write("\"{}\",{}\n".format(c[0], multiplier*int(c[1])))
 
 if __name__ == "__main__":
   main()
